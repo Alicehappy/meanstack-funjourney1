6a7d76a04417e82f37066353f9ea448bee8e889f
Alice Zou <alice@datafax.com>
2020-01-10 14:38:25 -0500

    Completed model and router -  have a working server API that lete me create, view, and delete the bucket list.

---
+++ models/List.js
@@ -0,0 +1,35 @@
+// Require mongoose package
+const mongoose = require('mongoose');
+
+// Define BucketlistSchema with title, description and category
+const BucketlistSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    category: {
+        type: String,
+        required: true,
+        enum: ['High', 'Medium', 'Low']
+    }
+});
+
+// Create a model using mongoose.model and export it
+const BucketList = module.exports = mongoose.model('BucketList', BucketlistSchema);
+
+// Bucketlist.find() returns all the lists
+module.exports.getAllLists = (callback) => {
+    BucketList.find(callback);
+}
+
+// newList.save is used to insert the document into MongoDB
+module.exports.addList = (newList, callback) => {
+    newList.save(callback);
+}
+
+// Here we need to pass an id parameter to BucketList.remove
+module.exports.deleteListById = (id, callback) => {
+    let query = {_id: id};
+    BucketList.remove(query, callback);
+}
